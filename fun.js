cdb7f4769f36df8e0849605914a25f71bca922ae
Thayer Hicks <thicks@unc.edu>
2023-06-20 11:10:52 -0400

    Added more support for smaller vp widths

--- fun.js
+++ fun.js
@@ -7,3 +7,25 @@ window.addEventListener('scroll', function() {
         navUl.classList.remove('scrolled');
     }
 });
+
+window.addEventListener('scroll', function() {
+    const top = document.querySelector('.top-section');
+    const bottom = document.querySelector('.bottom-section');
+    const topRect = top.getBoundingClientRect();
+    const bottomRect = bottom.getBoundingClientRect();
+    const viewportHeight = window.innerHeight;
+    bottom.style.position = 'relative';
+    if (bottomRect.top === viewportHeight) {
+        top.style.position = 'fixed';
+        top.style.bottom = 0 + 'px';
+        //bottom.style.paddingTop = viewportHeight + 'px';
+        bottom.style.top = (viewportHeight) + 'px';
+        console.log('fixing');
+    } else if (bottomRect.top > (viewportHeight)) {
+        top.style.position = 'static';
+        console.log('unfix');
+    } else {
+        console.log('rel');
+        //top.style.position = 'relative';
+    }
+});
