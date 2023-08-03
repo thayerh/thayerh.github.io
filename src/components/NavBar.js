596e71ce9bfb23787b46dcdbabb9d0cf1819c8ad
Thayer Hicks <thicks@unc.edu>
2023-08-03 15:20:00 -0700

    Reconfigured repo

---
+++ src/components/NavBar.js
@@ -0,0 +1,11 @@
+import NavIcon from "./NavIcon";
+
+const NavBar = () => {
+    return ( 
+        <div className="nav">
+            <NavIcon />
+        </div> 
+    );
+}
+ 
+export default NavBar;
